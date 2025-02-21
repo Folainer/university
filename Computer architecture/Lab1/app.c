109df57c5ee6f01cb0d46f2ae0c8ef625c1d9500
Maksym <mak.krutogouz@gmail.com>
2025-02-21 21:09:13 +0900

    computer architecture lab has been updated

--- Computer architecture/Lab1/app.c
+++ Computer architecture/Lab1/app.c
@@ -1,9 +1,3 @@
-// task 
-//Варіант No 12
-//Задано двовимірний масив (матрицю) цілих чисел A[m][n].
-//Відсортувати окремо кожен рядок масиву алгоритмом No3 методу
-//вставки (з лінійним пошуком справа з використанням бар’єру) за не-
-//збільшенням.
 
 #include <stdio.h>
 #include <time.h>
@@ -17,22 +11,22 @@ void printArray(int*, int, int);
 
 int main() {
     srand(time(NULL));
-    int A[M][N];
+    int A[M][N+1];
 
-    fillArray(&A[0][0], M, N);
+    fillArray(&A[0][0], M, N + 1);
 
-    printArray(&A[0][0], M, N);
+    printArray(&A[0][0], M, N + 1);
 
-    sortArray(&A[0][0], M, N);
+    sortArray(&A[0][0], M, N + 1);
 
-    printArray(&A[0][0], M, N);
+    printArray(&A[0][0], M, N + 1);
 
     return 0;
 }
 
 void fillArray(int *arr, int m, int n) {
     for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 1; j < n; j++) {
             arr[i * n + j] = rand() % 100;
         }
     }
@@ -40,25 +34,22 @@ void fillArray(int *arr, int m, int n) {
 
 void sortArray(int *arr, int m, int n) {
     for (int i = 0; i < m; i++) {
-        for (int j = 1; j < n; j++) {
-            int buf = arr[i*n + j];
-            int k = 0;
-            while (buf < arr[i*n + k]) {
-                k++;
+        for (int j = 2; j < n; j++) {
+            arr[i*n + 0] = arr[i*n + j];
+            int k = j;
+            while (arr[i*n + 0] < arr[i*n + k - 1]) {
+                arr[i* n + k] = arr[i*n + k - 1];
+                k--;
             }
 
-            for (int l = j - 1; l >= k; l--) {
-                arr[i * n + l + 1] = arr[i * n + l];
-            }
-
-            arr[i * n + k] = buf;
+            arr[i * n + k] = arr[i * n + 0];
         }
     }
 }
 
 void printArray(int *arr, int m, int n) {
     for (int i = 0; i < m; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 1; j < n; j++) {
             printf("%3d", arr[i * n + j]);
         }
         printf("\n");
--- Computer architecture/Lab1/sortArray.asm
+++ Computer architecture/Lab1/sortArray.asm
@@ -1,121 +1,107 @@
 sortArray:
         push    rbp
         mov     rbp, rsp
-        ; initialising local variables into a stack
-        mov     QWORD PTR [rbp-40], rdi
-        mov     DWORD PTR [rbp-44], esi
-        mov     DWORD PTR [rbp-48], edx
-        ; i loop condition is initialized : int i = 0
+        mov     QWORD PTR [rbp-24], rdi
+        mov     DWORD PTR [rbp-28], esi
+        mov     DWORD PTR [rbp-32], edx
         mov     DWORD PTR [rbp-4], 0
         jmp     .L9
-.L16:
-        ; i loop body starts
-        ; j loop condition initialisation : int j = 1
-        mov     DWORD PTR [rbp-8], 1
-        jmp     .L10
-.L15:
-        ; j loop body starts
-        ; it gets element into a buffer variable (to this line: mov DWORD PTR [rbp-20], eax) : int buf = arr[i*n + j];
-        ; firstly calculate shift in the array and then this shift adds with the shift from the start of the array
-        mov     eax, DWORD PTR [rbp-4]
-        imul    eax, DWORD PTR [rbp-48]
-        mov     edx, eax
-        mov     eax, DWORD PTR [rbp-8]
-        add     eax, edx
-        cdqe
-        lea     rdx, [0+rax*4]
-        mov     rax, QWORD PTR [rbp-40]
-        add     rax, rdx
-        mov     eax, DWORD PTR [rax]
-        mov     DWORD PTR [rbp-20], eax
-        ; set k to 0 : int k = 0;
-        mov     DWORD PTR [rbp-12], 0
-        jmp     .L11
-.L12:
-        ; k loop body (one command) : k++;
-        add     DWORD PTR [rbp-12], 1
-.L11:
-        ; k loop condition : buf < arr[i*n + k]
-        mov     eax, DWORD PTR [rbp-4]
-        imul    eax, DWORD PTR [rbp-48]
-        mov     edx, eax
-        mov     eax, DWORD PTR [rbp-12]
-        add     eax, edx
-        cdqe
-        lea     rdx, [0+rax*4]
-        mov     rax, QWORD PTR [rbp-40]
-        add     rax, rdx
-        mov     eax, DWORD PTR [rax]
-        cmp     DWORD PTR [rbp-20], eax
-        jl      .L12
-        ; l loop the condition variable is initialized : int l = j - 1;
-        mov     eax, DWORD PTR [rbp-8]
-        sub     eax, 1
-        mov     DWORD PTR [rbp-16], eax
-        jmp     .L13
 .L14:
-        ; k l loop body starts : arr[i * n + l + 1] = arr[i * n + l];
+        mov     DWORD PTR [rbp-8], 2
+        jmp     .L10
+.L13:
         mov     eax, DWORD PTR [rbp-4]
-        imul    eax, DWORD PTR [rbp-48]
+        imul    eax, DWORD PTR [rbp-32]
         mov     edx, eax
-        mov     eax, DWORD PTR [rbp-16]
+        mov     eax, DWORD PTR [rbp-8]
         add     eax, edx
         cdqe
         lea     rdx, [0+rax*4]
-        mov     rax, QWORD PTR [rbp-40]
+        mov     rax, QWORD PTR [rbp-24]
         lea     rcx, [rdx+rax]
         mov     eax, DWORD PTR [rbp-4]
-        imul    eax, DWORD PTR [rbp-48]
-        mov     edx, eax
-        mov     eax, DWORD PTR [rbp-16]
-        add     eax, edx
+        imul    eax, DWORD PTR [rbp-32]
         cdqe
-        add     rax, 1
         lea     rdx, [0+rax*4]
-        mov     rax, QWORD PTR [rbp-40]
+        mov     rax, QWORD PTR [rbp-24]
         add     rdx, rax
         mov     eax, DWORD PTR [rcx]
         mov     DWORD PTR [rdx], eax
-        ; k l loop body ends
-        ; l loop decrement : l--
-        sub     DWORD PTR [rbp-16], 1
-.L13:
-        ; l loop condition check : l >= k
-        mov     eax, DWORD PTR [rbp-16]
-        cmp     eax, DWORD PTR [rbp-12]
-        jge     .L14
-        ; seting element of array to a buffer value : arr[i * n + k] = buf;
+        mov     eax, DWORD PTR [rbp-8]
+        mov     DWORD PTR [rbp-12], eax
+        jmp     .L11
+.L12:
         mov     eax, DWORD PTR [rbp-4]
-        imul    eax, DWORD PTR [rbp-48]
+        imul    eax, DWORD PTR [rbp-32]
+        mov     edx, eax
+        mov     eax, DWORD PTR [rbp-12]
+        add     eax, edx
+        cdqe
+        sal     rax, 2
+        lea     rdx, [rax-4]
+        mov     rax, QWORD PTR [rbp-24]
+        lea     rcx, [rdx+rax]
+        mov     eax, DWORD PTR [rbp-4]
+        imul    eax, DWORD PTR [rbp-32]
         mov     edx, eax
         mov     eax, DWORD PTR [rbp-12]
         add     eax, edx
         cdqe
         lea     rdx, [0+rax*4]
-        mov     rax, QWORD PTR [rbp-40]
+        mov     rax, QWORD PTR [rbp-24]
         add     rdx, rax
-        mov     eax, DWORD PTR [rbp-20]
+        mov     eax, DWORD PTR [rcx]
         mov     DWORD PTR [rdx], eax
-        ; j loop condition increment : j++
+        sub     DWORD PTR [rbp-12], 1
+.L11:
+        mov     eax, DWORD PTR [rbp-4]
+        imul    eax, DWORD PTR [rbp-32]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rdx
+        mov     edx, DWORD PTR [rax]
+        mov     eax, DWORD PTR [rbp-4]
+        imul    eax, DWORD PTR [rbp-32]
+        mov     ecx, eax
+        mov     eax, DWORD PTR [rbp-12]
+        add     eax, ecx
+        cdqe
+        sal     rax, 2
+        lea     rcx, [rax-4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rax, rcx
+        mov     eax, DWORD PTR [rax]
+        cmp     edx, eax
+        jl      .L12
+        mov     eax, DWORD PTR [rbp-4]
+        imul    eax, DWORD PTR [rbp-32]
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        lea     rcx, [rdx+rax]
+        mov     eax, DWORD PTR [rbp-4]
+        imul    eax, DWORD PTR [rbp-32]
+        mov     edx, eax
+        mov     eax, DWORD PTR [rbp-12]
+        add     eax, edx
+        cdqe
+        lea     rdx, [0+rax*4]
+        mov     rax, QWORD PTR [rbp-24]
+        add     rdx, rax
+        mov     eax, DWORD PTR [rcx]
+        mov     DWORD PTR [rdx], eax
         add     DWORD PTR [rbp-8], 1
 .L10:
-        ; j loop condition condition check : j < n
         mov     eax, DWORD PTR [rbp-8]
-        cmp     eax, DWORD PTR [rbp-48]
-        jl      .L15
-        ; j loop ends
-        ; i loop condition increment : i++
+        cmp     eax, DWORD PTR [rbp-32]
+        jl      .L13
         add     DWORD PTR [rbp-4], 1
 .L9:
-        ; i loop condition check : i < m
         mov     eax, DWORD PTR [rbp-4]
-        cmp     eax, DWORD PTR [rbp-44]
-        jl      .L16
-        ; i loop ends
+        cmp     eax, DWORD PTR [rbp-28]
+        jl      .L14
         nop
         nop
         pop     rbp
         ret
-        ; function ends with no returning value
-.LC0:
-        .string "%3d"
